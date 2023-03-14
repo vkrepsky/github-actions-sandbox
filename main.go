373fdfde390ce88693642e412e612b8b429c4cb6
Victor Krepsky <victorwkrepsky@gmail.com>
2023-03-14 08:42:14 -0300

    test script

--- main.go
+++ main.go
@@ -2,9 +2,39 @@ package main
 
 import (
     "fmt"
-    "runtime"
+    "io"
+    "os"
 )
 
 func main() {
-    fmt.Println("Current OS:", runtime.GOOS)
+    filePath := "dummy.txt"
+    newDirPath := "dir"
+
+    if err := os.Mkdir(newDirPath, os.ModePerm); err != nil {
+        panic(err)
+    }
+
+    file, err := os.Open(filePath)
+    if err != nil {
+        panic(err)
+    }
+    defer file.Close()
+
+    newFilePath := fmt.Sprintf("%s/%s", newDirPath, file.Name())
+    newFile, err := os.Create(newFilePath)
+    if err != nil {
+        panic(err)
+    }
+    defer newFile.Close()
+
+    _, err = io.Copy(newFile, file)
+    if err != nil {
+        panic(err)
+    }
+
+    fmt.Printf("File %s moved to directory %s\n", filePath, newDirPath)
+
+    if err := os.Remove("dummy.txt"); err != nil {
+        panic(err)
+    }
 }
