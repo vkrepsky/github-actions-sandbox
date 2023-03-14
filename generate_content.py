9274b8f8e2b068e7a96bc49848f0fa9b2b68257d
Victor Krepsky <victorwkrepsky@gmail.com>
2023-03-14 10:46:25 -0300

    test script '?'

--- generate_content.py
+++ generate_content.py
@@ -3,7 +3,7 @@ import os
 def main():
     filename = "dummy.txt"
     with open(filename, "w") as f:
-        f.write("this is a generated content\n")
+        f.write(":) \n")
         f.close()
 
 if __name__ == "__main__":
